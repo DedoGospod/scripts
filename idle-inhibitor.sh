74b6e00e0990eac66c525a0361d9fb40fd01c8e0
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-05-20 16:53:52 +0800

    Update idle-inhibitor.sh

--- idle-inhibitor.sh
+++ idle-inhibitor.sh
@@ -6,12 +6,12 @@ HYPRIDLE_RUNNING=$(pgrep -x "hypridle")
 if [ -n "$HYPRIDLE_RUNNING" ]; then
     # hypridle is running, so stop it and stop sway-audio-idle-inhibit
     killall hypridle
-    killall "sway-audio-idle-inhibit" # Kill the sway-audio-idle-inhibit process
-    notify-send "Hypridle and sway-audio-idle-inhibit stopped."
+    killall "sway-audio-idle-inhibit"     # Kill the sway-audio-idle-inhibit process
+    notify-send "SLEEP DISABLED." -t 1000 # 1 second notification
 else
     # hypridle is not running, so start it and start sway-audio-idle-inhibit
     systemctl --user start --now hypridle.service
-    # Start sway-audio-idle-inhibit (assuming it's in your PATH)
+    # Start sway-audio-idle-inhibit 
     /usr/bin/sway-audio-idle-inhibit &
-    notify-send "Hypridle and sway-audio-idle-inhibit started."
+    notify-send "SLEEP ENABLED" -t 1000 # 1 second notification
 fi
