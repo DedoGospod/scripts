903e1374921495c02df934bae8ec2ab7c6ab5ef2
Dedo Gospod <dylanlazarov2002@protonmail.com>
2025-10-21 15:48:34 +0800

    Update btrfs-clean.sh

--- .local/bin/btrfs-clean.sh
+++ .local/bin/btrfs-clean.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-set -e
-
 sudo btrfs scrub start /
-sudo btrfs balance start -dusage=10 -musage=10 /
-
-
-# For automatic monthly btrfs cleaning, do: sudo mv /.local/bin/btrfs-clean.sh /etc/cron.monthly
-# Change the name of the file (cron doesn't run scripts with . in their filename) 
-# The command for this is: cd /etc/cron.monthly && sudo mv btrfs-clean.sh btrfs-clean-job
