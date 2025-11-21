1b34bfc042ef39cff38e9c55e91c7404fbeeb29c
DedoGospod <dylanlazarov2002@protonmail.com>
2025-11-21 16:38:07 +0800

    obs service fix

--- user-scripts/.local/bin/github.sh
+++ user-scripts/.local/bin/github.sh
@@ -18,7 +18,7 @@ fi
 
 # 2. Set the user.name globally
 git config --global user.name "$GIT_NAME"
-if git config --global user.email "$GIT_NAME"; then
+if git config --global user.name "$GIT_NAME"; then
     echo "✅ user.name set to $GIT_NAME"
 else
     echo "❌ Error setting user.name"
@@ -27,7 +27,7 @@ fi
 
 # 3. Set the credential.helper globally
 git config --global credential.helper "$GIT_HELPER"
-if git config --global user.email "$GIT_HELPER"; then
+if git config --global credential.helper "$GIT_HELPER"; then
     echo "✅ credential.helper set to $GIT_HELPER"
 else
     echo "❌ Error setting credential.helper"
--- user-scripts/.local/bin/idle-inhibitor.sh
+++ user-scripts/.local/bin/idle-inhibitor.sh
@@ -6,11 +6,11 @@ HYPRIDLE_RUNNING=$(pgrep -x "hypridle")
 if [ -n "$HYPRIDLE_RUNNING" ]; then
     # hypridle is running, so stop it and stop sway-audio-idle-inhibit
     systemctl --user stop --now hypridle.service
-    killall "sway-audio-idle-inhibit" # Kill the sway-audio-idle-inhibit process
+    killall "wayland-pipewire-idle-inhibit" # Kill the sway-audio-idle-inhibit process
     notify-send -t 1000 "SLEEP DISABLED."
 else
     # hypridle is not running, so start it and start sway-audio-idle-inhibit
     systemctl --user start --now hypridle.service
-    /usr/bin/sway-audio-idle-inhibit &
+    /usr/bin/wayland-pipewire-idle-inhibit &
     notify-send -t 1000 "SLEEP ENABLED"
 fi
