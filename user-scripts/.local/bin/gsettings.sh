bf788ef07a9e27de42d3ceaa57ccd92af3cb4ddb
DedoGospod <dylanlazarov2002@protonmail.com>
2025-11-25 21:31:47 +0800

    gsettings script

--- user-scripts/.local/bin/gsettings.sh
+++ user-scripts/.local/bin/gsettings.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# Show directories first
 echo "Changed the nautilus sort order to 'type'..."
 gsettings set org.gnome.nautilus.preferences default-sort-order 'type'   
+
+# Remove the 'recent' section from nautilus
 echo "Removed the 'recent' section from nautilus..."
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+# Set the color scheme to dark
+echo "Setting color-sceme to 'dark'..."
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
