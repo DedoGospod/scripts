2cb39210a1578c04f13c8f6af6efecea66bfe7e8
DedoGospod <dylanlazarov2002@protonmail.com>
2025-12-01 14:43:40 +0800

    idle inhibit script more robust

--- user-scripts/.local/bin/idle-inhibitor.sh
+++ user-scripts/.local/bin/idle-inhibitor.sh
@@ -6,11 +6,11 @@ HYPRIDLE_RUNNING=$(pgrep -x "hypridle")
 if [ -n "$HYPRIDLE_RUNNING" ]; then
     # hypridle is running, so stop it and stop sway-audio-idle-inhibit
     systemctl --user stop --now hypridle.service
-    killall "wayland-pipewire-idle-inhibit" # Kill the sway-audio-idle-inhibit process
+    systemctl --user stop --now wayland-pipewire-idle-inhibit.service
     notify-send -t 1000 "SLEEP DISABLED."
 else
     # hypridle is not running, so start it and start sway-audio-idle-inhibit
     systemctl --user start --now hypridle.service
-    /usr/bin/wayland-pipewire-idle-inhibit &
+    systemctl --user start --now wayland-pipewire-idle-inhibit.service
     notify-send -t 1000 "SLEEP ENABLED"
 fi
